bd61dc419b6aa1c9404b9ee2ca20ca2dbdd574d2
Nikita Kononenko <nikita.kononenko@omnigon.com>
2019-01-23 22:33:52 +0300

    Add DynArray blueprint, add Main with test helpers

---
+++ DynArray/src/DynArray.java
@@ -0,0 +1,39 @@
+public class DynArray<T>
+{
+    public T [] array;
+    public int count;
+    public int capacity;
+
+    public DynArray()
+    {
+        count = 0;
+        makeArray(16);
+    }
+
+    public void makeArray(int new_capacity)
+    {
+        // ваш код
+    }
+
+    public T getItem(int index)
+    {
+        // ваш код
+        return null;
+    }
+
+    public void append(T itm)
+    {
+        // ваш код
+    }
+
+    public void insert(T itm, int index)
+    {
+        // ваш код
+    }
+
+    public void remove(int index)
+    {
+        // ваш код
+    }
+
+}
---
+++ DynArray/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    /*
+        TEST helper methods
+     */
+
+    private static void test(boolean condition, String description) {
+        try {
+            check(condition, description);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static void check(boolean condition, String description) throws Exception {
+        if (!condition) {
+            throw new Exception(description);
+        }
+    }
+
+}
