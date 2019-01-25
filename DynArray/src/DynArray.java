cf8f599d1c0b3bb87d61f132a1e41cc1a40af4f8
Nikita Kononenko <nikita.kononenko@omnigon.com>
2019-01-25 20:35:20 +0300

    Add DynArray with all features implemented and tests written

--- DynArray/src/DynArray.java
+++ DynArray/src/DynArray.java
@@ -1,39 +1,153 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class DynArray<T>
 {
     public T [] array;
     public int count;
     public int capacity;
+    private Class<T> clazz;
 
-    public DynArray()
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final int CAPACITY_MULTIPLIER = 2;
+    private static final double CAPACITY_DENOMINATOR = 1.5;
+    private static final int OCCUPANCY_EDGE_PERCENTAGE = 50;
+
+    public DynArray(Class<T> clazz)
     {
-        count = 0;
-        makeArray(16);
+        this.clazz = clazz;
+        this.count = 0;
+        this.capacity = DEFAULT_CAPACITY;
+        makeArray(capacity);
     }
 
     public void makeArray(int new_capacity)
     {
         // ваш код
+        if (count != 0) {
+            this.capacity = new_capacity;
+            array = Arrays.copyOf(array, new_capacity);
+            return;
+        }
+        array = _makeArray(new_capacity);
     }
 
     public T getItem(int index)
     {
         // ваш код
-        return null;
+        if (isOutOfBounds(index)) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        return array[index];
     }
 
     public void append(T itm)
     {
         // ваш код
+        if (count == capacity) {
+            // increase capacity
+            increaseBuffer();
+            array = Arrays.copyOf(array, capacity);
+        }
+        array[count] = itm;
+        increaseCount();
     }
 
     public void insert(T itm, int index)
     {
         // ваш код
+        if (index <= -1 || (count > 0 && index > count)) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (index == count) {
+            append(itm);
+            return;
+        }
+
+        if (count == capacity) {
+            increaseBuffer();
+        }
+
+        T[] newArr = _makeArray(capacity);
+        System.arraycopy(array, 0, newArr, 0, index);
+        newArr[index] = itm;
+        int elementsCount = (count - index);
+        System.arraycopy(array, index, newArr, index + 1, elementsCount);
+        array = newArr;
+        increaseCount();
     }
 
     public void remove(int index)
     {
         // ваш код
+        if (isOutOfBounds(index)) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (occupancyPercentage(count - 1, capacity) < OCCUPANCY_EDGE_PERCENTAGE) {
+            decreaseBuffer();
+        }
+        T[] newArr = _makeArray(capacity);
+        System.arraycopy(array, 0, newArr, 0, index);
+        System.arraycopy(array, index + 1,newArr, index, capacity - index - 1);
+        decreaseCount();
+        array = newArr;
+    }
+
+    @SuppressWarnings("unchecked")
+    private T[] _makeArray(int capacity)
+    {
+        return (T[]) Array.newInstance(this.clazz, capacity);
+    }
+
+    private boolean isOutOfBounds(int index)
+    {
+        return index >= count || index <= -1;
+    }
+
+    private void increaseBuffer()
+    {
+        this.capacity = this.capacity * CAPACITY_MULTIPLIER;
+    }
+
+    private void increaseCount() {
+        count += 1;
+    }
+
+    private void decreaseBuffer()
+    {
+        if (capacity <= DEFAULT_CAPACITY) {
+            return;
+        }
+
+        int newCapacity = (int)(this.capacity / CAPACITY_DENOMINATOR);
+        if (newCapacity < DEFAULT_CAPACITY) {
+            newCapacity = DEFAULT_CAPACITY;
+        }
+        this.capacity = newCapacity;
+    }
+
+    private void decreaseCount() {
+        count -= 1;
+    }
+
+    private int occupancyPercentage(int count, int capacity)
+    {
+        return (100 * count) / capacity;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < count; i++) {
+            sb.append(array[i]);
+            sb.append((i == count -1) ? "" : ", ");
+        }
+        sb.append("]");
+        return sb.toString();
     }
 
 }
--- DynArray/src/Main.java
+++ DynArray/src/Main.java
@@ -1,7 +1,187 @@
 public class Main {
 
     public static void main(String[] args) {
+        testArrayCreatedWithProperType();
 
+        testArrayIsEmptyAfterCreation();
+        testMakeArrayCopiesArrayValues();
+        testAppendWorksProperly();
+
+        testInsertWorksProperly();
+        testInsertThrowsExceptionWhenIndexIsNegative();
+        testInsertThrowsExceptionWhenIndexIsMoreThanCount();
+        testInsertIncreasesBufferSizeWhenLimitReached();
+
+        testRemoveLeaveBufferSizeUnchanged();
+        testRemoveDecreaseBufferSize();
+        testRemoveThrowsExceptionWhenIndexIsNegative();
+        testRemoveThrowsExceptionWhenIndexIsMoreThanCount();
+    }
+
+    public static void testArrayCreatedWithProperType() {
+        DynArray<String> strings = new DynArray<>(String.class);
+        DynArray<Integer> ints = new DynArray<>(Integer.class);
+        DynArray<Double> doubles = new DynArray<>(Double.class);
+
+        Class cls = strings.array.getClass();
+        String stringsTypeName = cls.getSimpleName();
+        test(
+                stringsTypeName.equalsIgnoreCase("String[]"),
+                "Type of strings array should be String, but it's " + stringsTypeName
+        );
+
+        String intsTypeName = ints.array.getClass().getSimpleName();
+        test(
+                intsTypeName.equalsIgnoreCase("Integer[]"),
+                "Type of ints array should be Integer, but it's " + intsTypeName
+        );
+
+        String doublesTypeName = doubles.array.getClass().getSimpleName();
+        test(
+                doublesTypeName.equalsIgnoreCase("Double[]"),
+                "Type of doubles array should be Double, but it's " + doublesTypeName
+        );
+    }
+
+    private static void testArrayIsEmptyAfterCreation() {
+        DynArray<Integer> arr = new DynArray<>(Integer.class);
+        test(
+                arr.count == 0,
+                "Array should be empty"
+        );
+    }
+
+    private static void testMakeArrayCopiesArrayValues() {
+        DynArray<Integer> arr = new DynArray<>(Integer.class);
+        test(arr.count == 0, "Arr size should be 0");
+        test(arr.capacity == 16, "Arr capacity should be 16");
+
+        for (int i = 0; i < 4; i++) {
+            arr.append(i);
+        }
+        test(arr.count == 4, "Arr size should become 4");
+        test(arr.getItem(0) == 0, "First element should be 0");
+        test(arr.getItem(3) == 3, "Last element should be 4");
+        arr.makeArray(32);
+        test(arr.count == 4, "Arr size should remain 4");
+        test(arr.capacity == 32, "Arr capacity should become 32");
+    }
+
+    private static void testAppendWorksProperly() {
+        DynArray<Integer> arr = new DynArray<>(Integer.class);
+        for (int i = 0; i < 15; i++) {
+            arr.append(i * i);
+        }
+        int oldCapacity = arr.capacity;
+        test(arr.count == 15, "Array size should be 15");
+        test(arr.capacity == 16, "Capacity should not be changed until limit reached");
+
+        arr.append(1);
+        arr.append(2);
+        int newCapacity = oldCapacity * 2;
+        test(arr.count == 17, "Array size should become 17");
+        test(arr.capacity == newCapacity, "Capacity should become twice as big as previous value");
+    }
+
+    private static void testInsertWorksProperly() {
+        DynArray<Integer> arr = new DynArray<>(Integer.class);
+        arr.insert(2, 0);
+        test(arr.count == 1, "Arr size should become 1");
+        test(arr.getItem(0) == 2, "Item at 0 should be 2");
+        arr = new DynArray<>(Integer.class);
+
+        for (int i = 0; i < 15; i++) {
+            arr.append(2);
+        }
+        arr.insert(5, 1);
+        test(arr.count == 16, "Arr size should become 16");
+        test(arr.getItem(1) == 5, "Item should be inserted");
+
+        arr.insert(3, 1);
+        test(arr.count == 17, "Arr size should be 17");
+        test(arr.getItem(1) == 3, "Item should be inserted");
+        test(arr.getItem(2) == 5, "Items should be shifted");
+    }
+
+    private static void testInsertThrowsExceptionWhenIndexIsNegative() {
+        DynArray<Integer> arr = new DynArray<>(Integer.class);
+        try {
+            arr.insert(0, -1);
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Attempt to insert at negative index throws exception");
+        }
+    }
+
+    private static void testInsertThrowsExceptionWhenIndexIsMoreThanCount() {
+        DynArray<Integer> arr = new DynArray<>(Integer.class);
+        try {
+            arr.insert(0, 1);
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Attempt to insert at index that more than count throws exception");
+        }
+    }
+
+    private static void testInsertIncreasesBufferSizeWhenLimitReached() {
+        DynArray<Integer> arr = new DynArray<>(Integer.class);
+        int oldCapacity = arr.capacity;
+        int newCapacity = oldCapacity * 2;
+        for (int i = 0; i < 16; i++) {
+            arr.append(2);
+        }
+        test(arr.count == 16, "Arr size should be 16");
+        test(arr.capacity == oldCapacity, "Capacity should be the same");
+
+        arr.append(2);
+        test(arr.capacity == newCapacity, "Capacity should be increased");
+    }
+
+    private static void testRemoveLeaveBufferSizeUnchanged() {
+        DynArray<Integer> arr = new DynArray<>(Integer.class);
+        for (int i = 0; i < 25; i++) {
+            arr.append(i);
+        }
+        int oldCapacity = arr.capacity;
+        test(arr.count == 25, "Arr size should be 25");
+        arr.remove(0);
+        test(arr.count == 24, "Arr size should become 24");
+        test(arr.getItem(0) == 1, "First item should become 1");
+        arr.remove(5);
+        test(arr.getItem(5) == 7, "Fifth item shoud become 6");
+        test(arr.capacity == oldCapacity, "Capacity should be the same");
+    }
+
+    private static void testRemoveDecreaseBufferSize() {
+        DynArray<Integer> arr = new DynArray<>(Integer.class);
+        for (int i = 0; i < 33; i++) {
+            arr.append(i);
+        }
+
+        int oldCapacity = arr.capacity;
+        test(arr.count == 33, "Arr size should be 5");
+        arr.remove(0);
+        test(arr.count == 32, "Arr size should become 4");
+        test(arr.getItem(0) == 1, "First item should become 1");
+        arr.remove(5);
+        test(arr.getItem(5) == 7, "Fifth item shoud become 6");
+        test(arr.capacity < oldCapacity, "Capacity should be decreased");
+    }
+
+    private static void testRemoveThrowsExceptionWhenIndexIsNegative() {
+        DynArray<Integer> arr = new DynArray<>(Integer.class);
+        try {
+            arr.remove(-1);
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Attempt to remove item at negative index throws an exception");
+        }
+    }
+
+    private static void testRemoveThrowsExceptionWhenIndexIsMoreThanCount() {
+        DynArray<Integer> arr = new DynArray<>(Integer.class);
+        try {
+            arr.remove(80);
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Attempt to remove item at index bigger than array count throws an exception");
+        }
     }
 
     /*
