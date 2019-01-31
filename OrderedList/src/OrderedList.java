fd0a566f9b07501439ef8214df1dd51be3fdd413
Nikita Kononenko <nikita.kononenko@omnigon.com>
2019-01-31 13:53:36 +0300

    Remove BigInteger and BigDecimal numbers cast implementation (only Integer left)

--- OrderedList/src/OrderedList.java
+++ OrderedList/src/OrderedList.java
@@ -1,5 +1,3 @@
-import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.util.*;
 
 class Node<T>
@@ -162,35 +160,6 @@ public class OrderedList<T>
     }
 
     private int compareNumbers(Number x, Number y) {
-        if(isSpecial(x) || isSpecial(y))
-            return Double.compare(x.doubleValue(), y.doubleValue());
-        else
-            return toBigDecimal(x).compareTo(toBigDecimal(y));
-    }
-
-    private static boolean isSpecial(Number x) {
-        boolean specialDouble = x instanceof Double
-                && (Double.isNaN((Double) x) || Double.isInfinite((Double) x));
-        boolean specialFloat = x instanceof Float
-                && (Float.isNaN((Float) x) || Float.isInfinite((Float) x));
-        return specialDouble || specialFloat;
-    }
-
-    private static BigDecimal toBigDecimal(Number number) {
-        if(number instanceof BigDecimal)
-            return (BigDecimal) number;
-        if(number instanceof BigInteger)
-            return new BigDecimal((BigInteger) number);
-        if(number instanceof Byte || number instanceof Short
-                || number instanceof Integer || number instanceof Long)
-            return new BigDecimal(number.longValue());
-        if(number instanceof Float || number instanceof Double)
-            return new BigDecimal(number.doubleValue());
-
-        try {
-            return new BigDecimal(number.toString());
-        } catch(final NumberFormatException e) {
-            throw new RuntimeException("The given number (\"" + number + "\" of class " + number.getClass().getName() + ") does not have a parsable string representation", e);
-        }
+         return ((Integer)x).compareTo((Integer)y);
     }
 }
